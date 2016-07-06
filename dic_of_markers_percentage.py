d20d30543ab97eb952cac57cb8c576ef6e7cae12
Zifeng Mai <frederickmai@gmail.com>
2016-07-05 23:14:52 -0400

    new

---
+++ dic_of_markers_percentage.py
@@ -0,0 +1,88 @@
+# For the list in the key, [other_name, extracellular or intracellular, significance in identifying this cell type]
+
+astrocyte = {'CD40':['','ex',0],
+             'CD80':['','ex',0],
+             'CD86':['','ex',0],
+             'CD88':['','ex',0],
+             'GFAP':['','ex',0],
+             'S100B':['in',0],
+            }
+
+basophil = {'CD13':['','ex',0],
+            'CD44':['','ex',0],
+            'CD54':['','ex',0],
+            'CD63':['','ex',0],
+            'CD69':['','ex',0],
+            'CD107a':['','ex',0],
+            'CD123':['','ex',0],
+            'CD193':['CD193 (CCR3)','ex',0],
+            'CD203c':['','ex',0],
+            'FcεRIα':['','ex',0],
+            'IgE':['','ex',0],
+            'TLR4':['','ex',0],}
+
+total_t_cell = {'CD3':['','ex',100], 
+                'CD4':['','ex',0], 
+                'CD8':['','ex',0], 
+                'CD45':['','ex',0], 
+                'CD5':['','ex',0], 
+                'CD7':['','ex',0], 
+                'CD':['','ex',0],
+               }
+
+#Lin-1,Lin-2,Lin-3 are using same color
+dendritic_cell = {'CD11c':['','ex',100], 
+                  'CD16':['','ex',80],
+                  'CD56':['','ex',80],
+                  'CD80':['','ex',99],
+                  'CD83':['','ex',99],
+                  'CD86':['','ex',99],
+                  'CD123':['','ex',100],
+                  'CD209':['','ex',99], 
+                  'HLA-DR':['','ex',100], 
+                  'Lin-1':['','ex',100],
+                  'Lin-2':['','ex',100],
+                  'Lin-3':['','ex',100],
+                 }
+
+
+b_cell = {'CD5':['','ex',60], 
+          'CD19':['','ex',100], 
+          'CD20':['','ex',60], 
+          'CD23':['','ex',60], 
+          'CD38':['','ex',60], 
+          'CD45':['','ex',90], 
+          'FMC7':['','ex',60], 
+          'lg, ϰ light chain':['','ex',80], 
+          'lg, λ light chain':['','ex',80],
+         }
+
+myeloid_cell = {'CD11b':['','ex',80], 
+                'CD13':['','ex',100], 
+                'CD14':['','ex',70], 
+                'CD15':['','ex',100], 
+                'CD16':['','ex',100], 
+                'CD33':['','ex',33], 
+                'CD45':['','ex',100], 
+                'CD64':['','ex',99],
+               }
+
+'''t_reg = {'CD4':,
+         'CD25':,
+         'CD39':,
+         'CD62L':,
+         'CD73':,
+         'CD103':,
+         'CD134':,
+         'CD152 (CTLA-4)':,
+         'CD194 (CCR4)':,
+         'CD223':,
+         'FR4':,
+         'GARP':,
+         'GITR':,
+         'TGF-β':,
+         'FOXP3':,
+         'Helios':,
+        }
+'''
+
--- panelogic.py
+++ panelogic.py
@@ -1,7 +1,18 @@
-class panelogic(object):
+from dic_of_markers_percentage import *
+import random
+class Panelogic(object):
 	"""docstring for ClassName"""
-	def __init__(self, arg):
-		self.arg = arg
+	def __init__(self):
+		self.panel = [0]
 		
-	def ():
-		pass
+	def display():
+		return dic_of_markers_percentage.total_t_cell
+
+def main():
+	print ("Select cells, cell markers, or fluorophore you want to study")
+	panelogic = Panelogic()
+	print (panelogic.panel)
+	print (b_cell)
+
+if __name__ == '__main__':
+	main()
