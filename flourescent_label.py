4363398dd1ee888c625187e3d83de858119d7690
Zifeng Mai <frederickmai@gmail.com>
2016-09-20 08:16:35 -0700

    created flourescent label file

---
+++ flourescent_label.py
@@ -0,0 +1,5 @@
+# This file describe the flourescent label currently available
+# (label display name, brightness index, max excitation wavelength, max emission wave length)
+
+# Insert comment for brilliant_violet_421
+brilliant_violet_421 = ['Brilliant Violet 421™', 5, 405, 421]
